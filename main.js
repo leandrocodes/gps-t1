567852d5a3e046a8151045e6ecc16558ce376ac7
leandrocodes <leaxviana140@gmail.com>
2019-10-01 14:19:37 -0400

    rodizio de fotos para epléticos

---
+++ main.js
@@ -0,0 +1,82 @@
+let items = document.querySelectorAll('.item')
+
+
+setTimeout(() => {
+  setInterval(() => {
+    items[0].style.backgroundImage = "url('../img/bolo2.jpg')"
+  }, 3000)
+  
+  setInterval(() => {
+    items[0].style.backgroundImage = "url('../img/bolo1.jpg')"
+  }, 4000)
+}, 1000);
+
+setTimeout(() => {
+  setInterval(() => {
+    items[1].style.backgroundImage = "url('../img/bolo1.jpg')"
+  }, 3000)
+  
+  setInterval(() => {
+    items[1].style.backgroundImage = "url('../img/bolo3.jpg')"
+  }, 4000)
+}, 1000);
+
+setTimeout(() => {
+  setInterval(() => {
+    items[2].style.backgroundImage = "url('../img/bolo3.jpg')"
+  }, 3000)
+  
+  setInterval(() => {
+    items[2].style.backgroundImage = "url('../img/bolo4.jpg')"
+  }, 4000)
+}, 1000);
+
+setTimeout(() => {
+  setInterval(() => {
+    items[3].style.backgroundImage = "url('../img/bolo4.jpg')"
+  }, 3000)
+  
+  setInterval(() => {
+    items[3].style.backgroundImage = "url('../img/bolo6.jpg')"
+  }, 4000)
+}, 1000);
+
+setTimeout(() => {
+  setInterval(() => {
+    items[4].style.backgroundImage = "url('../img/bolo5.jpg')"
+  }, 3000)
+  
+  setInterval(() => {
+    items[4].style.backgroundImage = "url('../img/bolo3.jpg')"
+  }, 4000)
+}, 1000);
+
+setTimeout(() => {
+  setInterval(() => {
+    items[5].style.backgroundImage = "url('../img/bolo6.jpg')"
+  }, 3000)
+  
+  setInterval(() => {
+    items[5].style.backgroundImage = "url('../img/bolo4.jpg')"
+  }, 4000)
+}, 1000);
+
+setTimeout(() => {
+  setInterval(() => {
+    items[6].style.backgroundImage = "url('../img/bolo7.jpg')"
+  }, 3000)
+  
+  setInterval(() => {
+    items[6].style.backgroundImage = "url('../img/bolo8.jpg')"
+  }, 4000)
+}, 1000);
+
+setTimeout(() => {
+  setInterval(() => {
+    items[7].style.backgroundImage = "url('../img/bolo9.jpg')"
+  }, 3000)
+  
+  setInterval(() => {
+    items[7].style.backgroundImage = "url('../img/bolo7.jpg')"
+  }, 4000)
+}, 1000);
